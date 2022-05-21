014ef697f8cb1aeec9ed74afb54097e1ceb4c489
Your Name <you@example.com>
2022-05-21 14:13:48 +0530

    screenvalue updated

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let screen = document.getElementById("screen")
 let  buttons = document.querySelectorAll("button")
-let screenvalue = ""
-for(items of buttons){
+let screenvalue = ""//empty string
+for(items of buttons){ //items is iterator
     items.addEventListener('click',(e)=>{
 
-buttonText = e.target.innerText
+buttonText = e.target.innerText //grabbing the buttons innertext
 console.log("button text is " ,buttonText)
 
+//conditions on button text
 if(buttonText==="X"){
-    buttonText = "*"
+    buttonText = "*"//setting buttontext to *
     screenvalue  +=buttonText 
     screen.value = screenvalue
 }
@@ -18,12 +19,14 @@ else if (buttonText==="C"){
 }
 else if (buttonText==="="){
   screen.value = eval(screenvalue)
+  screenvalue = ""
+//empty the value in screenvariable
 }
 else{
   screenvalue += buttonText
   screen.value = screenvalue
 }
-
+// screenvalue variable is continsly updated on clicking the button intialy it is empty
 
 
 
